6dfee9c5a171a2e793940d66a82acf6d0305bd86
CamDavi5 <camrdavis.biz@gmail.com>
2022-07-27 16:34:40 -0500

    improved suppliers page

--- src/pages/Suppliers.jsx
+++ src/pages/Suppliers.jsx
@@ -8,11 +8,41 @@ const Suppliers = () => {
         "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", 
         "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+    let supplierArr = ["ALFA CUTTING TOOLS",
+        "AUVECO PRODUCTS",
+        "BOSS GLOVE",
+        "BRIGHTON BEST INTERNATIONAL",
+        "CAMEL GRINDING WHEELS",
+        "CHAMPION CUTTING TOOLS",
+        "COILHOSE PNEUMATICS",
+        "CORDOVA SAFETY PRODUCTS",
+        "DRILLCO, INC.",
+        "DURHAM MANUFACTURING",
+        "G.L. HUYETT",
+        "GLOBAL ABRASIVE PRODUCTS",
+        "IFASTGROUPE",
+        "INTERNATIONAL FASTENERS",
+        "JET LUBE, INC.",
+        "LINDSTROM METRIC",
+        "NORD-LOCK WASHER",
+        "RADIAN SAFETY PRODUCTS",
+        "SAS SAFETY",
+        "SMITH COOPER/SHARPE VALVE",
+        "STAR STAINLESS SCREW CO.",
+        "UNITED ABRASIVES",
+        "WARRENSVILLE FILE",
+        "WESTERN WIRE",
+        "WROUGHT WASHER",
+        "XL SCREW CORP"];
     
-    
+    function buttonTop () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
+
     return (
         <>
-            <nav id="productSpyScroll"className="navbar navbar-light bg-light d-flex justify-content-center">
+            <nav id="productSpyScroll"className="navbar navbar-light bg-light d-flex justify-content-center mb-3">
                 <ul className="nav nav-pills">
                     {alphabetArr.map((char) => (
                         <ScrollSpyNav letter={char}></ScrollSpyNav>
@@ -22,13 +52,17 @@ const Suppliers = () => {
             <div data-spy="scroll" data-target="#productSpyScroll" data-offset="0">
                 {alphabetArr.map((char) => (
                     <>
-                        <div className="d-flex">
-                            <h5 id={char}>{char}</h5>
-                            <button className="ml-3">To Top</button>
-                        </div>
+                        {supplierArr.some((supplier) => (
+                                supplier[0] === char      
+                        )) ? <div className="d-flex ml-2">
+                                <h5 id={char}>{char}</h5>
+                                <button className="ml-3" onClick={buttonTop}>To Top</button>
+                            </div> : null}  
+                        
                         <ul>
-                            {alphabetArr.map((char) => (
-                                <h6>Poggers</h6>
+                            {supplierArr.map((supplier) => (
+                                supplier[0] == char
+                                ? <ScrollSpySupplier item={supplier}></ScrollSpySupplier> : null
                             ))}
                         </ul>
                     </>
@@ -37,7 +71,7 @@ const Suppliers = () => {
             </div>
         </>
 
-    )
+    );
 }
 
 export default Suppliers;
